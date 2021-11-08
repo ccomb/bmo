0f95b6fd91dad6af17f0264f905c4fddaf5da58a
Christophe Combelles <ccomb@free.fr>
2021-11-08 20:48:58 +0100

    prod env

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+if [ "$1" == "" ]; then echo "Need the target host as 1st argument"; exit 1;fi
+if [ "$2" == "" ]; then echo "Need the root mongo password as 2nd argument"; exit 1;fi
 ./build.sh -o
-ssh $1 "cd services/bmo && git pull && /opt/bin/docker-compose up -d --build"
+ssh $1 "ssh-keyscan git.anybox.cloud >> ~/.ssh/known_hosts && cd services/bmo && git pull && MONGO_INITDB_ROOT_PASSWORD=$2 /opt/bin/docker-compose up -d --build"
