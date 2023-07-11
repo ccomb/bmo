2dac6e95e51ed861f98223bd4d09ea95ce509632
Christophe Combelles <ccomb@free.fr>
2023-07-11 08:12:26 +0200

    updated deployment path

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 if [ "$1" == "" ]; then echo "Need the target host as 1st argument"; exit 1;fi
 if [ "$2" == "" ]; then echo "Need the root mongo password as 2nd argument"; exit 1;fi
-ssh $1 "cd services/bmo && git pull && MONGO_INITDB_ROOT_PASSWORD=$2 /opt/bin/docker-compose up -d --build"
+ssh -p 222 $1 "cd services/prelab/bmo && git pull && MONGO_INITDB_ROOT_PASSWORD=$2 /opt/bin/docker-compose up -d --build"
